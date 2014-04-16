cc6d1fecb90e8174e4ff7b0550ddb0a52fe4c50b
Reggie Johnson <rjohnson@boardvantage.com>
2014-04-16 11:42:14 -0700

    video player implementation 2x's

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php include 'sections/header.php'; ?>
 <!-- ########## EDIT BELOW PER PAGE ########## -->
-		  
+<style>
+
+</style>		  
 <!-- slider hero -->
     <section class="slider-hero" id="index-hero"> 
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0">Board of Directors</li><!--
+    <li data-target="#carousel-example-generic" data-slide-to="0">Boards of Directors</li><!--
     --><li data-target="#carousel-example-generic" data-slide-to="1" class="active">Senior Leadership</li><!--
     --><li data-target="#carousel-example-generic" data-slide-to="2">Dispersed Teams</li>
   </ol>
@@ -14,30 +16,55 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner">
     <div class="item">
-      <img src="img/banner-director.png" alt="Director secure board portal for paperless boardroom">
+      <img class="img-responsive carousel-banner" src="img/banner-director.png" alt="Director secure board portal for paperless boardroom">
       <div class="container">
-      <div class="carousel-caption">
-        <img class="banner-text" src="img/director-banner-text337x162.png" alt="Director text">
-        <a href="products/director.php"><img class="banner-button" src="img/orange-learn-button180x50.png" alt="orange learn button"></a>
+      <div class="carousel-caption-holder pull-left">
+        <div class="col-md-6 col-sm-6 nopadding">
+        <img class="banner-text img-responsive" src="img/director-banner-text337x162.png" alt="Director text">
+      </div>
+      <div class="col-md-12 col-sm-12 nopadding">
+        <a href="products/director.php"><img class="img-responsive banner-button" src="img/orange-learn-button180x50.png" alt="orange learn button"></a>
+      </div>
       </div> <!--caption-->
     </div><!--container -->
     </div>
     <div class="item active">
-      <img src="img/banner-meetx.png" alt="MeetX secure collaboration for senior leadership">
+      <img class="img-responsive carousel-banner" src="img/banner-meetx.png" alt="MeetX secure collaboration for senior leadership">
       <div class="container">
-      <div class="carousel-caption">
-        <img class="banner-text" src="img/meetx-banner-text307x166.png" alt="MeetX text">
-        <a href="products/meetx.php"><img class="banner-button" src="img/orange-learn-button180x50.png" alt="orange learn button"></a>
-        <a href="http://vimeo.com/69575441" target="new"><img class="banner-button" src="img/blue-play-button180x50.png" alt="blue play button"></a>
+      <div class="carousel-caption-holder pull-left">
+        <div class="col-md-6 col-sm-6 nopadding">
+        <img class="banner-text img-responsive" src="img/meetx-banner-text307x166.png" alt="MeetX text">
+      </div>
+      <div class="col-md-12 col-sm-12 nopadding">
+          <a href="products/meetx.php"><img class="banner-button img-responsive" src="img/orange-learn-button180x50.png" alt="orange learn button"></a>
+   <!-- *****************     Code below relates to popup for Video Player   ****  include css and jquery files     *******   -->
+ 		<link rel="stylesheet" href="../stylesheets/colorbox.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="../js/jquery.colorbox.js"></script>
+		<script>
+			var Video1 = $.noConflict();  //this is used where have 2 jquery functions using doc ready
+			Video1(document).ready(function(){
+				Video1(".vimeo").colorbox({iframe:true, innerWidth:800, innerHeight:450});
+				});
+		</script>
+					
+			<a class='vimeo' href="http://player.vimeo.com/video/69575441?autoplay=1" title=""><img class="img-responsive banner-button" src="img/blue-play-button180x50.png" alt="blue play button"></a>
+            		
+	<!-- *****************     END Video Player   ****           *******   -->
+          </div>
         </div> <!--caption-->
     </div><!--container -->
     </div>
     <div class="item">
-      <img src="img/banner-sharex.png" alt="ShareX secure file sharing for the mobile enterprise">
+      <img class="img-responsive carousel-banner" src="img/banner-sharex.png" alt="ShareX secure file sharing for the mobile enterprise">
       <div class="container">
-      <div class="carousel-caption">
-        <img class="banner-text" src="img/sharex-banner-text314x165.png" alt="ShareX text">
-        <a href="products/sharex.php"><img class="banner-button" src="img/orange-learn-button180x50.png" alt="orange learn button"></a>
+      <div class="carousel-caption-holder pull-left">
+        <div class="col-md-6 col-sm-6 nopadding">
+        <img class="banner-text img-responsive" src="img/sharex-banner-text314x165.png" alt="ShareX text">
+      </div>
+      <div class="col-md-12 col-sm-12 nopadding">
+        <a href="products/sharex.php"><img class="img-responsive banner-button" src="img/orange-learn-button180x50.png" alt="orange learn button"></a>
+      </div>
     </div> <!--caption-->
     </div><!--container -->
     </div>
@@ -48,7 +75,7 @@
     <!-- ##### CONTENT ##### -->
     <div class="row page-lead one-nav">
       <div class="container">
-        <p class="lead col-xs-10 col-xs-offset-1 text-center">BoardVantage leads the market in delivering paperless process for board and leadership communication. Trusted by enterprise worldwide, BoardVantage sets the bar for a secure and mobile executive experience.
+        <p class="lead col-xs-12 text-center nopadding">BoardVantage leads the market in delivering paperless process for board and leadership communication. Trusted by enterprise worldwide, BoardVantage sets the bar for a secure and mobile executive experience.
         </p>
       </div>
     </div>
@@ -57,46 +84,94 @@
           <div class="col-xs-12 page-content">
             <div class="row feat-product">
               <div class="col-sm-3 col-xs-12">
-                <img class="img-responsive" src="img/sharex-screen.png">
+                <a href="https://sharex.com/login/login.jsp" target="new"><img class="img-responsive" src="img/sharex-screen.png"></a>
               </div>
               <div class="col-sm-9 col-xs-12">
-                <h2 class="blue">Announcing ShareX for Dispersed Teams in the Mobile Enterprise</h2>
+                 <h2 class="blue">Announcing ShareX for Dispersed Teams in the Mobile Enterprise</h2>
                 <p>When content is uploaded to the cloud, organizations are at risk of losing control of confidential information. ShareX restores that control while still meeting the user's needs for mobility and a rich user experience. ShareX preserves the benefits of ready access to updated content, without the worry of information leaks.</p>
-                <a href="https://sharex.com/login/login.jsp" class="learn-more-link" target="new">Learn More &gt;</a>
+                <a href="../products/sharex.php" class="read-more-link">Learn More &gt;</a>
               </div>
             </div>
+            
             <hr class="extended"/>
+          </div><!-- / .page-content -->
+        </div><!-- / container -->
+        
+    <div class="row page-wrap">
+      <div class="container">
+          <div class="col-xs-12 page-content">
+            <div class="row feat-product">
+              <div class="col-sm-3 col-xs-12">
+              <a href="http://www.boardvantage.com/downloads/white-papers/boardvantage-wp-leadership-communication-mobile-enterprise.pdf" target="new"><img class="img-responsive image-shadow" src="img/196x251_PBCL_Cover.png"></a>
+            </div>
+            <div class="col-sm-8 col-sm-offset-1 col-xs-12">
+              <h5>White Paper</h5>
+              <h2 class="blue">Transforming Leadership Communication<br />- Key Insights for the Mobile Enterprise</h2>
+              <p>Dispersion for leadership teams is a fact of life in the mobile enterprise. Those teams have communication needs that go beyond email and paper. This white paper has practical insights about how to enable better leadership team collaboration using technology.</p>
+              <a href="http://www.boardvantage.com/downloads/white-papers/boardvantage-wp-leadership-communication-mobile-enterprise.pdf" class="read-more-link" target="new">Download &gt;</a>
+              </div>
+            </div>
+            
+            <hr class="extended"/>
+          </div><!-- / .page-content -->
+        </div><!-- / container -->
+
+    <div class="row page-wrap">
+      <div class="container">
+          <div class="col-xs-12 page-content">
+            <div class="row feat-product">
+              <div class="col-sm-3 col-xs-12">
+              <a href="resources/board-portals-a-primer.php"><img class="img-responsive image-shadow" src="img/COVER_BP.png"></a>
+            </div>
+            <div class="col-sm-8 col-sm-offset-1 col-xs-12">
+              <h2 class="blue" style="margin-top:0;">Board Portal: A Primer</h2>
+              <p>
+                A board portal is a secure app or website designed explicitly for the purpose of facilitating communication between directors and the company. The current generation of board portals supports information exchange and captures the process that takes place in meetings and in between meetings. There are three major attributes in a board portal: a) online access for directors, b) toolkit for the corporate secretary and c) security which meets or exceeds the standards of the IT organization. Directors access the board portal from a browser or an iPad but the iPad is overwhelmingly the device of choice today.
+              </p>
+              <a href="resources/board-portals-a-primer.php" class="read-more-link">Learn more about Board Portals &gt;</a>
+              </div>
+            </div>
+            
+            <hr class="extended"/>
+          </div><!-- / .page-content -->
+        </div><!-- / container -->
+        
+    <div class="row informational" style="margin-bottom: 50px;">
+        <div class="container">
+          <div class="col-xs-12 page-content">
+            <div class="row featured-news">
+              <div class="col-xs-12">
             <div class="row social-feeds">
-              <div class="col-xs-4">
+              <div class="col-xs-4 homepage-triad">
                 <div class="row">
                   <div class="col-xs-3 icon-area">
                     <img src="img/orange-speaker.png" alt="Spotlight">
                   </div>
-                  <div class="col-xs-7">
+                  <div class="col-xs-8">
                     <h4 class="social-header">In the Spotlight</h4>
                   </div>
                 </div>
-                <a href="#">
-                  <p>CEOCFO magazine interview with Joe Ruck, CEO of BoardVantage</p>
-                  <p class="sub-text">CEOCFO</p>
+                <a href="http://www.ceocfointerviews.com/interviews/BoardVantage13.htm" target="_blank">
+                  CEOCFO magazine interview with Joe Ruck, CEO of BoardVantage<br>
+                  <span class="sub-text">CEOCFO</span>
                 </a>
                 <hr />
-                <a href="#">
-                  <p>Extending Mobile Collaboration Reach</p>
-                  <p class="sub-text">IT Business Edge</p>
+                <a href="http://www.itbusinessedge.com/blogs/it-unmasked/boardvantage-extends-mobile-collaboration-reach.html" target="_blank">
+                  Extending Mobile Collaboration Reach<br>
+                  <span class="sub-text">IT Business Edge</span>
                 </a>
                 <hr />
-                <a href="#">
-                  <p>Consumer File-Sharing Services</p>
-                  <p class="sub-text">Wall Street Journal</p>
+                <a href="http://online.wsj.com/article/SB10001424127887323826804578466511705102932.html?KEYWORDS=forrester" target="_blank">
+                  Consumer File-Sharing Services<br>
+                  <span class="sub-text">Wall Street Journal</span>
                 </a>
                 <hr />
-                <a href="#">
-                  <p>Making the Leap to the Paperless Boardroom</p>
-                  <p class="sub-text">Fund Directions</p>
+                <a href="/data-resources/articles/FD-Making The Leap.pdf" target="_blank">
+                  Making the Leap to the Paperless Boardroom<br>
+                  <span class="sub-text">Fund Directions</span>
                 </a>
               </div>
-              <div class="col-xs-4">
+              <div class="col-xs-4 homepage-triad">
                 <div class="row">
                   <div class="col-xs-3 icon-area">
                     <img src="img/orange-ipad.png" alt="Blog">
@@ -105,29 +180,28 @@
                     <h4 class="social-header">Blog</h4>
                   </div>
                 </div>
-                <a href="#">
-                  <p>Don't Let Wi-Fi Issues Let You Down</p>
-                  <p class="sub-text">March 3, 2014</p>
+                <a href="http://blog.boardvantage.com/2014/03/03/dont-let-wi-fi-issues-let-you-down/" target="_blank">
+                  Don't Let Wi-Fi Issues Let You Down<br>
+                  <span class="sub-text">March 3, 2014</span>
                 </a>
                 <hr />
-                <a href="#">
-                  <p>Announcing ShareX for Dispersed Teams in the Mobile Enterprise</p>
-                  <p class="sub-text">February 20, 2014</p>
+                <a href="http://blog.boardvantage.com/2014/02/20/announcing-sharex-for-dispersed-teams-in-the-mobile-enterprise/" target="_blank">
+                  Announcing ShareX for Dispersed Teams in the Mobile Enterprise<br>
+                  <span class="sub-text">February 20, 2014</span>
                 </a>
                 <hr />
-                <a href="#">
-                  <p>Creating an Executive Experience</p>
-                  <p class="sub-text">December 18, 2013</p>
+                <a href="http://blog.boardvantage.com/2013/12/18/creating-an-executive-experience/" target="_blank">
+                  Creating an Executive Experience<br>
+                  <span class="sub-text">December 18, 2013</span>
                 </a>
                 <hr />
-                <a href="#">
-                  <p>Producer-Consumer Model</p>
-                  <p class="sub-text">December 6, 2013</p>
+                <a href="http://blog.boardvantage.com/2013/12/06/producer-consumer-model/" target="_blank">
+                  Producer-Consumer Model<br>
+                  <span class="sub-text">December 6, 2013</span>
                 </a>
                 <hr />
-                  <p><a class="learn-more-link" href="http://blog.boardvantage.com/" target="_blank">More posts &gt;</a></p>
               </div>
-              <div class="col-xs-4">
+              <div class="col-xs-4 homepage-triad">
                 <div class="row">
                   <div class="col-xs-3 icon-area">
                     <img src="img/orange-twitter.png" alt="Twitter">
@@ -136,42 +210,27 @@
                     <h4 class="social-header">Tweets</h4>
                   </div>
                 </div>
-                <p>Last day of @NRECANews @TechAdvantage1 2014! Great seeing our many clients and prospects turning out despite the snow! See you next year!</p>
+                <a href="#">Last day of @NRECANews @TechAdvantage1 2014! Great seeing our many clients and prospects turning out despite the snow! See you next year!</a>
                 <hr />
-                <p>Wi-Fi or not, BoardVantage ShareX makes all your shared files accessible at all times. http://blog.boardvantage.com/2014/03/03/dont-let-wi-fi-issues-let-you-down/ …</p>
+                <a href="#">Wi-Fi or not, BoardVantage ShareX makes all your shared files accessible at all times. http://blog.boardvantage.com/2014/03/03/dont-let-wi-fi-issues-let-you-down/ …</a>
                 <hr />
-                <p>Meeting many clients at the 2014 NRECA TechAdvantage conference. Stop by and let us know how you use BoardVantage!</p>
+                <a href="#">Meeting many clients at the 2014 NRECA TechAdvantage conference. Stop by and let us know how you use BoardVantage!</a>
                 <hr />
-                  <p><a class="learn-more-link" href="https://twitter.com/boardvantage" target="_blank">Follow us on Twitter &gt;</a></p>
               </div>
             </div>
-            <hr class="extended"/>
-          </div><!-- / .page-content -->
-        </div><!-- / container -->
-        
-        <div class="row informational">
-          <div class="container">
-            <div class="col-sm-3 col-xs-12">
-              <img class="img-responsive" src="img/white-paper_key-insights.jpg">
-            </div>
-            <div class="col-sm-8 col-sm-offset-1 col-xs-12">
-              <h5>White Paper</h5>
-              <h2 class="blue">Transforming Leadership Communication<br />- Key Insights for the Mobile Enterprise</h2>
-              <p>Dispersion for leadership teams is a fact of life in the mobile enterprise. Those teams have communication needs that go beyond email and paper. This white paper has practical insights about how to enable better leadership team collaboration using technology.</p>
-              <a href="http://www.boardvantage.com/downloads/white-papers/boardvantage-wp-leadership-communication-mobile-enterprise.pdf" class="learn-more-link" target="new">Download &gt;</a>
-            </div>
           </div>
-        </div>
-
-        <div class="container">
+           <div class="container">
           <div class="col-xs-12 page-content">
-            <div class="row featured-news">
-              <div class="col-xs-12">
-                <h2 class="blue">Board Portal: A Primer</h2>
-                <p>A board portal is a secure app or website designed explicitly for the purpose of facilitating communication between directors and the company. The current generation of board portals supports information exchange and captures the process that takes place in meetings and in between meetings. There are three major attributes in a board portal: a) online access for directors, b) toolkit for the corporate secretary and c) security which meets or exceeds the standards of the IT organization. Directors access the board portal from a browser or an iPad but the iPad is overwhelmingly the device of choice today.</p>
-                <a href="products/board-portals-a-primer.php" class="learn-more-link">Learn more about Board Portals &gt;</a>
-                <p>&nbsp;</p>
-                <hr class="extended"/>
+            <div class="col-xs-4">
+            </div>
+              <div class="pull-down col-xs-4">
+                  <p><a class="read-more-link" href="http://blog.boardvantage.com/" target="_blank">More posts &gt;</a></p>
+                </div><!--/pull-down -->
+                <div class="pull-down col-xs-4">
+                <p><a class="read-more-link" href="https://twitter.com/boardvantage" target="_blank">Follow us on Twitter &gt;</a></p>
+                </div><!--/pull-down -->
+          </div><!--/col-xs-12-->
+        </div><!--/container-->
           </div>
         </div>
         </div>
@@ -184,7 +243,7 @@
   
     <script>
     $('.carousel').carousel({
-      interval: 4000
+      interval: 5000
     })
     </script>
   </body>
